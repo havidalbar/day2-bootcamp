cadbe94227c9af69fc0af07945dd5cadae441f5c
havidalbar <havidalbar@gmail.com>
2024-08-21 13:16:54 +0700

    Init Project

---
+++ Services/Services/MemberService.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess.Models;
+using DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class MemberService
+    {
+        public readonly static MemberService Instance = new MemberService();
+        private readonly UserRepository userRepository = UserRepository.Instance;
+
+        static MemberService() { }
+
+        public void AddNewMember(string username, string password)
+        {
+            var newMember = new Member()
+            {
+                Username = username,
+                Password = password
+            };
+
+            userRepository.Save(newMember);
+        }
+
+        public void DisableMember(string username)
+        {
+            var userToDisable = userRepository.GetByUsername(username);
+
+            if (userToDisable is null || userToDisable is not Member)
+                return;
+
+            var user = (Member)userToDisable;
+            user.IsDisabled = true;
+            userRepository.Save(user);
+        }
+    }
+}
---
+++ UserManagement/UserInterfaces/AdminPage.cs
@@ -0,0 +1,79 @@
+﻿using Services.Dtos;
+using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace UserManagement.UserInterfaces
+{
+    public static class AdminPage
+    {
+        private static LoginService loginService = LoginService.Instance;
+        private static MemberService memberService = MemberService.Instance;
+        private static LoginInfo loginInfo = loginService.GetLoginInfo();
+
+        private static List<string> options = new List<string>()
+        {
+            "Add member account",
+            "Disable member account",
+            "Logout"
+        };
+
+        public static void Start()
+        {
+            while (loginInfo.IsLoggedIn)
+            {
+                var choice = Helpers.GetChoice(options);
+
+                if (choice == options.Count)
+                {
+                    loginService.Logout();
+                    Console.WriteLine("You've logged out.");
+                    continue;
+                }
+
+                if (choice == 1)
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter member username:");
+                        var username = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.WriteLine("Enter member password:");
+                        var password = Console.ReadLine();
+
+                        if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                            continue;
+
+                        memberService.AddNewMember(username, password);
+                    }
+                    catch(Exception ex)
+                    {
+                        continue;
+                    }
+                }
+
+                if (choice == 2)
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter member username:");
+                        var username = Console.ReadLine();
+                        Console.WriteLine();
+                        
+                        memberService.DisableMember(username);
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
